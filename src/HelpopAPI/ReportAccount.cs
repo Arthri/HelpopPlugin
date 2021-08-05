98dbb7f3b9e069cf8a8699c1e967869c401cac54
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-06 00:00:52 +0800

    feat: Add basic project

---
+++ src/HelpopAPI/ReportAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace HelpopAPI
+{
+    /// <summary>
+    /// Represents the account of a <see cref="ReportUser"/>
+    /// </summary>
+    public class ReportAccount
+    {
+        /// <summary>
+        /// Represents the account's ID
+        /// </summary>
+        public int ID { get; }
+
+        /// <summary>
+        /// Represents the account's name
+        /// </summary>
+        public string Name { get; }
+
+        public ReportAccount(
+            int id,
+            string name)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
---
+++ src/HelpopAPI/ReportUser.cs
@@ -0,0 +1,40 @@
+﻿namespace HelpopAPI
+{
+    /// <summary>
+    /// Represents a user that made a report
+    /// </summary>
+    public class ReportUser
+    {
+        /// <summary>
+        /// Represents the user's character name
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Represents the user's IP
+        /// </summary>
+        public string IP { get; }
+
+        /// <summary>
+        /// Represents the user's UUID
+        /// </summary>
+        public string UUID { get; }
+
+        /// <summary>
+        /// Represents the user's server account
+        /// </summary>
+        public ReportAccount UserAccount { get; }
+
+        public ReportUser(
+            string name,
+            string ip,
+            string uuid,
+            ReportAccount userAccount)
+        {
+            Name = name;
+            IP = ip;
+            UUID = uuid;
+            UserAccount = userAccount;
+        }
+    }
+}
